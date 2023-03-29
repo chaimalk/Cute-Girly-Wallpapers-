09db7dcd501b5800c9577a0ebe67ec3d6f65dcf7
chaimalk <108950943+chaimalk@users.noreply.github.com>
2023-03-29 20:49:06 +0200

    Create Cute-Girly-Wallpapers.ads

---
+++ Cute-Girly-Wallpapers.ads
@@ -0,0 +1,13 @@
+{
+  "AdsController": {
+    "adNetwork": "unity",
+    "admobBanner": "ca-app-pub-3863740059386376/2487023345",
+    "admobInterstitial": "ca-app-pub-3863740059386376/8860860009",
+    "applovinBanner": "42b3b2b8e00912fa",
+    "applovinInterstitial": "20537cc292651120",
+    "unityGameId": "5225549",
+    "testMode": false,
+    "unityBanner": "Banner_Android",
+    "unityInterstitial": "Interstitial_Android"
+  }
+}
